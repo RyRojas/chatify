9465adb765ea1632868a84415b49764a233a27fb
RyRojas <30281811+RyRojas@users.noreply.github.com>
2021-06-23 15:28:18 -0700

    Implemented GiftedChat interface

--- components/chat.jsx
+++ components/chat.jsx
@@ -1,18 +1,61 @@
-import React from 'react';
-import { StyleSheet, ScrollView, Text } from 'react-native';
+import React, { useEffect, useState, useCallback } from 'react';
+import { View, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
 export default function Chat({ navigation, route }) {
+  //Props from user input on welcome screen
   const { name, bgColor } = route.params;
 
+  let [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello Developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 2,
+        text: 'This is a system message',
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
+  }, []);
+
+  const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+  }, []);
+
+  //Custom renderBubble function to change wrapper (speech bubble) color
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{ right: { backgroundColor: '#000' } }}
+      />
+    );
+  };
+
   //Update title with user input -- useEffect to avoid component update warnings
-  React.useEffect(
-    () => navigation.setOptions({ title: name }),
-    [navigation, name]
-  );
+  useEffect(() => navigation.setOptions({ title: name }), [navigation, name]);
 
   return (
-    <ScrollView style={{ backgroundColor: bgColor }}>
-      <Text>You made it to Screen2!</Text>
-    </ScrollView>
+    <View style={{ flex: 1, backgroundColor: bgColor }}>
+      <GiftedChat
+        renderBubble={renderBubble}
+        messages={messages}
+        onSend={(messages) => onSend(messages)}
+        user={{ _id: 1 }}
+      />
+    </View>
   );
 }
